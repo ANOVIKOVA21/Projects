91147d7d7b42ed487d6623bc82e715e825a0d863
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-10-20 18:06:07 +0300

    feat: add background images and slider for them

--- momentum/js/main.js
+++ momentum/js/main.js
@@ -2,6 +2,11 @@ const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const nameInput = document.querySelector('.name');
+const body = document.querySelector('body');
+const btnNext = document.querySelector('.slide-next');
+const btnPrev = document.querySelector('.slide-prev');
+
+let randomNum;
 
 function showTime() {
     const date = new Date();
@@ -24,7 +29,7 @@ function showDate() {
 function getTimeOfDay() {
     const date = new Date();
     const hours = date.getHours();
-    const timesOfDay = ['night', 'morning', 'day', 'evening']
+    const timesOfDay = ['night', 'morning', 'afternoon', 'evening']
     return timesOfDay[Math.floor(hours / 6)]
 }
 
@@ -45,3 +50,35 @@ function getLocalStorage() {
         nameInput.value = localStorage.getItem('name');
     }
 }
+
+function getRandomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomNum(1, 20)
+
+function setBg() {
+    const timeOfDay = getTimeOfDay()
+    let bgNum = randomNum + ''
+    bgNum.length === 1 ? bgNum = '0' + randomNum : randomNum
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/ANOVIKOVA21/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+    img.onload = () => {
+        body.style.backgroundImage = `url('${img.src}')`
+        console.log(randomNum)
+    }
+}
+setBg()
+
+function getSlideNext() {
+    randomNum === 20 ? randomNum = 1 : randomNum++
+        setBg()
+}
+
+function getSlidePrev() {
+    randomNum === 1 ? randomNum = 20 : randomNum--
+        setBg()
+}
+btnNext.addEventListener('click', getSlideNext)
+btnPrev.addEventListener('click', getSlidePrev)
