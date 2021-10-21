d41ba5756934a5e7478fa2b68b923d274fe40f22
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-10-21 16:00:40 +0300

    feat: add weather widget and quotes

--- momentum/js/main.js
+++ momentum/js/main.js
@@ -5,6 +5,16 @@ const nameInput = document.querySelector('.name');
 const body = document.querySelector('body');
 const btnNext = document.querySelector('.slide-next');
 const btnPrev = document.querySelector('.slide-prev');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const weatherError = document.querySelector('.weather-error');
+const city = document.querySelector('.city');
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const btnChangeQuote = document.querySelector('.change-quote');
 
 let randomNum;
 
@@ -43,11 +53,15 @@ window.addEventListener('load', getLocalStorage)
 
 function setLocalStorage() {
     localStorage.setItem('name', nameInput.value);
+    localStorage.setItem('city', city.value);
 }
 
 function getLocalStorage() {
     if (localStorage.getItem('name')) {
         nameInput.value = localStorage.getItem('name');
+    }
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
     }
 }
 
@@ -82,3 +96,40 @@ function getSlidePrev() {
 }
 btnNext.addEventListener('click', getSlideNext)
 btnPrev.addEventListener('click', getSlidePrev)
+
+
+//Weather
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=df45b93f4e1254acbda8ce8b11d4f1af&units=metric`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${Math.round(data.main.temp)}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        wind.textContent = `${Math.round(data.wind.speed)} m/s`
+        humidity.textContent = `${data.main.humidity}%`
+        weatherError.textContent = ''
+            // console.log(data);
+    } catch (er) {
+        er = 'Please enter correct data'
+        weatherError.textContent = er
+    }
+}
+getWeather()
+city.addEventListener('change', getWeather)
+
+async function getQuotes() {
+    const quotes = 'js/quotes.json';
+    const res = await fetch(quotes);
+    const data = await res.json();
+    // console.log(data[5].text)
+    getRandomNum(0, data.length - 1)
+    const randomNumOfQuote = randomNum;
+    console.log(randomNum + ' quote')
+    quote.textContent = data[randomNumOfQuote].text
+    author.textContent = data[randomNumOfQuote].author
+}
+getQuotes();
+btnChangeQuote.addEventListener('click', getQuotes)
