309670aeff1aac31a71176aa42830886f83094a8
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-12-16 21:56:16 +0200

    refactor: delete spaces and change some expressions

--- christmas-task/src/ts/sliders.ts
+++ christmas-task/src/ts/sliders.ts
@@ -5,31 +5,31 @@ const sliderQuantity = <target>document.querySelector('.filters-by-range__quanti
 const sliderQuantityValues = document.querySelectorAll('.filters-by-range__quantity-value');
 
 noUiSlider.create(sliderQuantity, {
-    start: [1, 12],
-    connect: true,
-    range: {
-        min: 1,
-        max: 12,
-    },
-    step: 1,
+  start: [1, 12],
+  connect: true,
+  range: {
+    min: 1,
+    max: 12,
+  },
+  step: 1,
 });
 
 sliderQuantity.noUiSlider.on('update', function (values, handle) {
-    sliderQuantityValues[handle].innerHTML = (parseInt(values[handle] as string) as unknown) as string;
+  sliderQuantityValues[handle].innerHTML = parseInt(values[handle] as string).toString();
 });
 
 const sliderYear = <target>document.querySelector('.filters-by-range__year');
 const sliderYearValues = document.querySelectorAll('.filters-by-range__year-value');
 noUiSlider.create(sliderYear, {
-    start: [1940, 2020],
-    connect: true,
-    range: {
-        min: 1940,
-        max: 2020,
-    },
-    step: 1,
+  start: [1940, 2020],
+  connect: true,
+  range: {
+    min: 1940,
+    max: 2020,
+  },
+  step: 1,
 });
 
 sliderYear.noUiSlider.on('update', function (values, handle) {
-    sliderYearValues[handle].innerHTML = (parseInt(values[handle] as string) as unknown) as string;
+  sliderYearValues[handle].innerHTML = parseInt(values[handle] as string).toString();
 });
