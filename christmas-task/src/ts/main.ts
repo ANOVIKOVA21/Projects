aed6ded560560eb53b7ace5642f3424a27db63b1
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-12-19 21:18:42 +0200

    feat: add function for select cards and function for warning

--- christmas-task/src/ts/main.ts
+++ christmas-task/src/ts/main.ts
@@ -1,2 +1,43 @@
 import '../styles.css';
-alert('hello');
+import data from './data';
+import { createCard } from './toysCards';
+import './sliders';
+import { sortToys } from './filters';
+
+let chosenToy = 0;
+
+function showWarning(parent: HTMLDivElement) {
+  const warningEl = document.createElement('div');
+  warningEl.classList.add('warning');
+  warningEl.textContent = 'Извините, все слоты заполнены';
+  parent.appendChild(warningEl);
+  setTimeout(() => {
+    warningEl.remove();
+  }, 3000);
+}
+document.addEventListener('DOMContentLoaded', function () {
+  const cardsContainer = document.querySelector('.toys-page__cards') as HTMLDivElement;
+  const sortData = sortToys(data);
+  console.log(sortData);
+  sortData.forEach((toy) => {
+    const card = createCard(toy);
+    cardsContainer.appendChild(card);
+  });
+
+  cardsContainer.addEventListener('click', (ev: Event) => {
+    const card = (ev.target as HTMLElement).closest('.card');
+
+    if (card != null) {
+      const selectedBallsEl = document.querySelector('.selected-balls__num') as HTMLSpanElement;
+      card.classList.toggle('card-active');
+      if (card.classList.contains('card-active')) {
+        if (chosenToy === 20) {
+          showWarning(cardsContainer);
+          card.classList.remove('card-active');
+        } else chosenToy++;
+      } else chosenToy--;
+
+      selectedBallsEl.textContent = chosenToy.toString();
+    }
+  });
+});
