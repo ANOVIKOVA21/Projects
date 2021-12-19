9a0593c2b325f8783cb4d6ed140c49c96ce8d2b8
ANOVIKOVA21 <anna2013novikova@gmail.com>
2021-12-19 21:16:52 +0200

    feat: add reset filters button

--- christmas-task/src/ts/sliders.ts
+++ christmas-task/src/ts/sliders.ts
@@ -1,5 +1,6 @@
 import noUiSlider, { target } from 'nouislider';
 import 'nouislider/dist/nouislider.css';
+import { filters, updateToysCards } from './filters';
 
 const sliderQuantity = <target>document.querySelector('.filters-by-range__quantity');
 const sliderQuantityValues = document.querySelectorAll('.filters-by-range__quantity-value');
@@ -16,6 +17,9 @@ noUiSlider.create(sliderQuantity, {
 
 sliderQuantity.noUiSlider.on('update', function (values, handle) {
   sliderQuantityValues[handle].innerHTML = parseInt(values[handle] as string).toString();
+  filters.quantity[handle] = parseInt(values[handle] as string);
+  console.log(filters);
+  updateToysCards();
 });
 
 const sliderYear = <target>document.querySelector('.filters-by-range__year');
@@ -32,4 +36,29 @@ noUiSlider.create(sliderYear, {
 
 sliderYear.noUiSlider.on('update', function (values, handle) {
   sliderYearValues[handle].innerHTML = parseInt(values[handle] as string).toString();
+  filters.years[handle] = parseInt(values[handle] as string);
+  console.log(filters);
+  updateToysCards();
+});
+
+const resetBtn = document.querySelector('.toys-page__reset') as HTMLButtonElement;
+resetBtn.addEventListener('click', () => {
+  const forms: NodeListOf<HTMLElement> = document.querySelectorAll('.filters-by-values__form');
+  const colors: NodeListOf<HTMLElement> = document.querySelectorAll('.filters-by-values__color');
+  const sizes: NodeListOf<HTMLElement> = document.querySelectorAll('.filters-by-values__size');
+  const favorite = document.getElementById('favorite') as HTMLInputElement;
+  forms.forEach((el) => {
+    if (el.classList.contains('form-active')) el.click();
+  });
+  colors.forEach((el) => {
+    if (el.classList.contains('color-active')) el.click();
+  });
+  sizes.forEach((el) => {
+    if (el.classList.contains('size-active')) el.click();
+  });
+  if (favorite.classList.contains('favorite-active')) favorite.click();
+  sliderQuantity.noUiSlider.set([1, 12]);
+  sliderYear.noUiSlider.set([1940, 2020]);
+
+  updateToysCards();
 });
