6a2afdf8ee1f8a3490e4d6513970cbe44319887d
Anna <anna2013novikova@gmail.com>
2023-06-27 18:49:55 +0300

    fix: add promise  to safely pause the video, add video events instead setInterval

--- museum-dom/src/js/video.js
+++ museum-dom/src/js/video.js
@@ -10,34 +10,71 @@ const btnVolumeImg = document.querySelector('.video__volume img');
 const btnFullscreen = document.querySelector('.video__fullscreen');
 const btnFullscreenImg = document.querySelector('.video__fullscreen img');
 
-let interval;
+let promise;
 function videoPause(currentVideo, videoState) {
   btnPlay.dataset.isPlay = 'false';
   if (videoState === 'pause') currentVideo.pause();
-  clearInterval(interval);
   btnPlayLarge.style.display = '';
+  btnPlayLarge.children[0].src = './svg/play_large.svg';
   btnPlayImg.src = './svg/play_small.svg';
 }
 function videoPlay(currentVideo) {
   debugger;
-  btnPlay.dataset.isPlay = 'true';
   if (videoTrack.value === '100') videoTrack.value = '0';
-  btnPlayLarge.style.display = 'none';
+  if (currentVideo.readyState >= 3) promise = currentVideo.play();
+  else {
+    currentVideo.load();
+    currentVideo.addEventListener('canplay', () => {
+      console.log('canplay readyState', currentVideo.readyState);
+      promise = currentVideo.play();
+    });
+  }
+
   btnPlayImg.src = './svg/pause.svg';
-  currentVideo.play();
-  interval = setInterval(() => {
+  const playingListener = function () {
+    btnPlay.dataset.isPlay = 'true';
+    btnPlayLarge.style.display = 'none';
+  };
+
+  const waitingListener = function () {
+    btnPlayLarge.children[0].src = './gif/loading.gif';
+    btnPlayLarge.style.display = '';
+  };
+  const timeListener = function () {
+    if (currentVideo !== document.querySelector('.video__video.slick-active'))
+      return;
     if (videoTrack.value !== '100') {
+      if (currentVideo.readyState < 3) return;
       const videoTime = Math.round(currentVideo.currentTime);
       const videoLength = Math.round(currentVideo.duration);
       videoTrack.value = `${(videoTime * 100) / videoLength}`;
       videoTrack.style.background = `linear-gradient(to right, #710707 0%, #710707 ${videoTrack.value}%, #C4C4C4 ${videoTrack.value}%, #C4C4C4 100%)`;
-    } else videoPause(currentVideo, 'ended');
-  }, 10);
+    } else {
+      videoPause(currentVideo, 'ended');
+      currentVideo.removeEventListener('timeupdate', timeListener, false);
+      currentVideo.removeEventListener('playing', playingListener);
+      currentVideo.removeEventListener('waiting', waitingListener);
+    }
+  };
+  currentVideo.addEventListener('playing', playingListener);
+  currentVideo.addEventListener('waiting', waitingListener);
+  currentVideo.addEventListener('timeupdate', timeListener, false);
 }
+
 export function handleVideo() {
   const currVideo = document.querySelector('.video__video.slick-active');
-  if (btnPlay.dataset.isPlay === 'false') videoPlay(currVideo);
-  else videoPause(currVideo, 'pause');
+  if (btnPlay.dataset.isPlay === 'false') {
+    videoPlay(currVideo);
+    console.log('play ', promise);
+  } else {
+    console.log('pause ', promise);
+    if (promise !== undefined) {
+      promise.then(() => {
+        videoPause(currVideo, 'pause');
+      });
+    }
+    return false;
+  }
 }
 function volumeIsMute() {
   if (btnVolume.dataset.isMute === 'true') {
