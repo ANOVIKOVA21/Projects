9a36133f8323db76e8d5a833d8a0851078f5ebc9
Anna <anna2013novikova@gmail.com>
2023-09-07 11:04:49 +0300

    fix: improve menu open function

--- museum-dom/src/js/menu.js
+++ museum-dom/src/js/menu.js
@@ -4,26 +4,22 @@ export function addMenuListeners() {
   const welcomeContent = document.querySelector('.welcome__content');
 
   menuButton.addEventListener('click', () => {
-    if (menuButton.dataset.isOpen === 'true') {
-      menuButton.dataset.isOpen = false;
-      menu.style.transform = 'translateX(-100%)';
+    if (menu.classList.contains('-open')) {
       menuButton.style.background =
         'url(./svg/burger.svg) no-repeat right transparent';
-
-      setTimeout(() => {
-        menu.hidden = true;
-        welcomeContent.style.opacity = '1';
-      }, 500);
+      welcomeContent.style.opacity = '1';
     } else {
-      menuButton.dataset.isOpen = true;
-      menu.hidden = false;
+      welcomeContent.style.opacity = '0';
       menuButton.style.background =
         'url(./svg/close-white.svg) no-repeat right transparent';
-
-      setTimeout(() => {
-        menu.style.transform = 'translateX(0)';
-        welcomeContent.style.opacity = '0';
-      }, 1000);
+    }
+    menu.classList.toggle('-open');
+    document.body.classList.toggle('lock');
+  });
+  menu.addEventListener('click', (ev) => {
+    const { target } = ev;
+    if (menu.classList.contains('-open') && target.tagName === 'A') {
+      menuButton.click();
     }
   });
 }
